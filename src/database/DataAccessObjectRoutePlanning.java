7f54b41ed45fc7d3680d80ac4830626300537763
zimnyKamil <kamiloz7@o2.pl>
2015-04-24 02:24:30 +0200

    Poprawienie i dodanie nowych funkcjonalnosci w wizualizacji producentow oraz wizualizacji trasy.

---
+++ src/database/DataAccessObjectRoutePlanning.java
@@ -0,0 +1,47 @@
+package database;
+
+import java.util.ArrayList;
+
+import exceptions.DatabaseConnectionExeption;
+
+public class DataAccessObjectRoutePlanning 
+{
+
+	private DatabaseConnector databaseConnector;
+	
+	public DataAccessObjectRoutePlanning()
+	{
+		databaseConnector = new DatabaseConnector();
+	}
+	/**
+	 * Metoda zwracajaca tablice wspolrzednych geograficznych miasta o nazwie
+	 * podanej w parametrze.
+	 * @return NULL OR String [] tab :
+	 * <br>tab[0] -> dlugosc geograficzna 
+	 * <br>tab[1] -> szerokosc geograficzna
+	 * @author Kamil Zimny
+	 */
+	public String [] getCityCoordinates(final String cityName)
+	{
+		String query = "SELECT dlugosc,szerokosc FROM Miasta WHERE nazwa_miasta = '"+ cityName +"';";
+		String [] coordinates = null;
+		ArrayList<ArrayList<Object>> resultOfQuery = null;
+		try 
+		{
+			resultOfQuery = databaseConnector.getResultOfMySqlQuery(query,2);
+			coordinates = new String[2];
+		} 
+		catch (DatabaseConnectionExeption e) 
+		{
+			e.printStackTrace();
+		}
+		
+		if( resultOfQuery != null && coordinates != null && resultOfQuery.size() > 0)
+		{
+			coordinates[0] = (String)resultOfQuery.get(0).get(0);
+			coordinates[1] = (String)resultOfQuery.get(0).get(1);
+		}
+		
+		return coordinates;
+	}
+}
---
+++ src/jpanels/DiscriptionOnMapJPanel.java
@@ -0,0 +1,65 @@
+package jpanels;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
+
+public class DiscriptionOnMapJPanel extends JPanel 
+{
+	private static final long serialVersionUID = 1L;
+	private JScrollPane scrollPane;
+	private JTextArea txtStatus;
+	  
+	public DiscriptionOnMapJPanel() 
+	{
+		// status UI to display the selected features
+	    txtStatus = new JTextArea();
+	    txtStatus.setText("Nazwy zaznaczonych producentów.");
+	    txtStatus.setLineWrap(true);
+	    txtStatus.setWrapStyleWord(true);
+	    txtStatus.setFont(new Font(txtStatus.getFont().getName(), txtStatus.getFont().getStyle(), 12));
+	    txtStatus.setBackground(Color.BLACK);
+	    txtStatus.setForeground(Color.WHITE);
+	    txtStatus.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+	    txtStatus.setEditable(false);
+
+	    // description
+	    JTextArea description = new JTextArea(
+	        "Kliknij na obiekty by zaznaczyæ producenta.");
+	    description.setForeground(Color.WHITE);
+	    description.setBackground(new Color(0, 0, 0, 80));
+	    description.setEditable(false);
+	    description.setLineWrap(true);
+	    description.setWrapStyleWord(true);
+	    description.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+	    description.setSize(200, 60);
+	    description.setMaximumSize(new Dimension(200, 60));
+
+	    // group the above UI items into a panel
+	    BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+	    setLayout(boxLayout);
+	    setLocation(10, 10);
+	    setSize(200, 190);
+	    setBackground(new Color(0, 0, 0, 80));
+	    setBorder(new LineBorder(Color.BLACK, 5, false));
+
+	    // Scroll pane for reporting features
+	    scrollPane = new JScrollPane(txtStatus);
+
+	    add(description);
+	    add(scrollPane);
+	}
+	
+	public JTextArea getDiscriptionArea()
+	{
+		txtStatus.setText("");
+		return txtStatus;
+	}
+}
