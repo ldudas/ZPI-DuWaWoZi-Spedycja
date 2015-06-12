170e9b16e49e93868af337764201989958c5e918
Łukasz Dudaszek <dudidudek93@gmail.com>
2015-06-12 11:16:57 +0200

    Ostateczny commit do zmiany kolorów

--- src/visualisations/Transporters/VisualisationTransportersModel.java
+++ src/visualisations/Transporters/VisualisationTransportersModel.java
@@ -88,6 +88,19 @@ public class VisualisationTransportersModel
 		size_category = null;
 	}
 	
+	public void clearData()
+	{
+		if( transporters != null)
+			transporters.clear();
+		if( transporters_filtered != null)
+			transporters_filtered.clear();
+		if( maps_cache != null)
+			maps_cache.clear();
+		cityFrom="";
+		cityTo="";
+		size_category = null;
+	}
+	
 	private void getTranspotersFormDatabase(String city_from, String city_to)
 	{
 		//pobieranie listy przewoźnikow z bazy 
